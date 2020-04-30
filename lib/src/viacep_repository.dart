085be74dfbf5f89bfc74e9f77e90e590b00eefaf
Bruno Sil <deltafid@gmail.com>
2020-04-29 21:03:10 -0300

    Via_Cep_Search

---
+++ lib/src/viacep_repository.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:via_cep_search/src/model.dart';
+
+class ViaCepRepository {
+  
+Future<ViaCepSearch> getViacep(String cep) async {
+   var response = await Dio().get("https://viacep.com.br/ws/$cep/json/");
+      return ViaCepSearch.fromJson(response.data);
+    
+  }
+
+}
